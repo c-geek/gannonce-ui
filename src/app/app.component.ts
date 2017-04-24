a0d02b15cc9f6740be9bad3a28a5a25b9a505547
cgeek <cem.moreau@gmail.com>
2017-04-24 14:14:28 +0200

    Identification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, ViewChild} from '@angular/core';
 
-import {MenuController, Nav} from 'ionic-angular';
+import {AlertController, MenuController, Nav, ToastController} from 'ionic-angular';
 
 import { HelloIonicPage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
@@ -13,14 +13,21 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   // make HelloIonicPage the root (or first) page
+  pub:string;
+  estIdentifie:Boolean;
   site_pages: Array<{title: string, url: string, component: any}>;
   perso_pages: Array<{title: string, url: string, component: any}>;
 
   constructor(
     // public platform: Platform
-    public menu: MenuController
+    public menu: MenuController,
+    public alertCtrl: AlertController,
+    public toastCtrl: ToastController
   ) {
     this.initializeApp();
+
+    this.pub = localStorage.getItem('pub')
+    this.estIdentifie = !!this.pub
 
     // set our app2's pages
     this.site_pages = [
@@ -39,5 +46,55 @@ export class MyApp {
     // this.statusBar.styleDefault();
     // this.splashScreen.hide();
     // });
+  },
+
+  showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Identification',
+      message: "Collez ci-dessous une clé publique de porte-monnaie Ğ1.",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Clé publique "a4ce87z..."'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Annuler',
+          handler: data => {
+            this.cleIncorrecte("Aucune clé saisie. Opération annulée.")
+          }
+        },
+        {
+          text: 'Valider',
+          handler: data => {
+            if (!data.title) {
+              this.cleIncorrecte("Aucune clé saisie. Opération annulée.")
+            }
+            else if (!data.title.match(/[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{43,44}/)) {
+              this.cleIncorrecte("Échec d'identification : '" + data.title + "' n'est pas une une clé publique valide.")
+            } else {
+              localStorage.setItem('pub', data)
+              this.estIdentifie = true
+              this.cleIncorrecte("Identification réussie.")
+            }
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  cleIncorrecte(message) {
+    let toast = this.toastCtrl.create({
+      message,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  disconnect() {
+    this.estIdentifie = false
+    this.cleIncorrecte("Déconnexion réussie.")
   }
 }
